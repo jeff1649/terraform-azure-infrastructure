e945ff1dbad62726e0a85a8cee0cf60c66989f7f
jeff1649 <jeff1649@yahoo.com>
2026-09-09 15:10:36 -0500

    Move networking to dedicated resource group

--- main.tf
+++ main.tf
@@ -30,10 +30,21 @@ module "networking" {
   subscription        = var.subscription
   instance            = var.vnet_instance
   location            = var.location
-  resource_group_name = module.resource_group.name
+  resource_group_name = module.network_resource_group.name
 
   address_space = var.vnet_address_space
   subnets       = var.subnets
 
   tags = var.tags
+}
+
+module "network_resource_group" {
+  source = "git::https://github.com/jeff1649/azurerm-resource-group.git?ref=v1.1.0"
+
+  app_abbreviation = "network"
+  environment      = var.environment
+  subscription     = var.subscription
+  instance         = 1
+  location         = var.location
+  tags             = var.tags
 }
